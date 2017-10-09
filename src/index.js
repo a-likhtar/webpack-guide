99166bff4b92e6c0d73ff46114b30952d94310a6
Andrey Likhtar <andrey.likhar@gmail.com>
2017-10-09 15:13:43 +0300

    Task 9 (Lazy Loading)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function getComponent() {
-  return import(/* webpackChunkName: "lodash" */'lodash').then(_ => {
-    var element = document.createElement('div');
+import _ from 'lodash';
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-  }).catch(error => 'An error occured while loading the component');
+function component() {
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+    print();  
+  });
+
+  return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+document.body.appendChild(component());
